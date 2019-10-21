3c5a41f569112024199881aa19ef936c3d9cf3bb
hereak <adam.krzeminski14@gmail.com>
2019-10-21 16:52:46 +0200

    Lab2

--- Zad3.scala
+++ Zad3.scala
@@ -15,11 +15,4 @@ object Zad3 extends App {
       case _ => crossAppendRec(list1, list2)
     }
   }
-
-  println(crossAppend(List(1, 3, 5, 7, 9, 11), List(2, 4, 6, 8)))
-  println(crossAppend(List(10, 8, 6, 4, 2, 0), List(9, 7, 5, 3, 1, -1, -3, -5)))
-  println(crossAppend(List(), List(9, 7, 5, 3, 1)))
-  println(crossAppend(List(10, 8, 6, 4, 2, 0), List()))
-  println(crossAppend(List(1), List(2)))
-  println(crossAppend(List(), List()))
 }
