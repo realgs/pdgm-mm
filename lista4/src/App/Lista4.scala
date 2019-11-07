406847b6d05d6b9164ca9ac6bb8bb72b9a85d5d5
mprzymus <pzrymus@gmail.com>
2019-11-07 18:59:25 +0100

    Lista4

--- lista4/src/App/Lista4.scala
+++ lista4/src/App/Lista4.scala
@@ -1,5 +1,30 @@
 package App
 
 class Lista4 {
-
+  def filter[A](list : List[List[A]],op: A) : List[List[A]] = {
+    def filterHelper(list : List[List[A]],element: A, result : List[List[A]]) : List[List[A]] = {
+      if (list == Nil) result
+      else if (list.head.contains(element)) filterHelper(list.tail,element, list.head::result)
+      else filterHelper(list.tail,element,result)
+    }
+    filterHelper(list,op,List()).reverse
+  }
+  def decToHex(number : Int) : List[Int] = {
+    def decToHexHelper(number : Int, acc : List[Int]) : List[Int] = {
+      val result = number / 16
+      val rest = number % 16
+      if (result == 0) rest::acc
+      else decToHexHelper(result,rest::acc)
+    }
+    decToHexHelper(number, List())
+  }
+  def decToAny(number : Int, convertTo : Int) : List[Int] = {
+    def decToHexHelper(number : Int, acc : List[Int]) : List[Int] = {
+      val result = number / convertTo
+      val rest = number % convertTo
+      if (result == 0) rest::acc
+      else decToHexHelper(result,rest::acc)
+    }
+    decToHexHelper(number, List())
+  }
 }
