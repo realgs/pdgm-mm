d6f4bec12a504321ae3d4b5a17d6304d29f5734c
rafal1behrendt@gmail.com <rafal1behrendt@gmail.com>
2019-11-13 20:08:57 +0100

    Added duplicates substitution, added creating tree from list

--- lista5/lista5.scala
+++ lista5/lista5.scala
@@ -1,6 +1,11 @@
 sealed trait BT[+A]
 case object Empty extends BT[Nothing]
-case class Node[+A](elem: A, left:BT[A], right:BT[A]) extends BT[A]
+case class Node[+A](elem: Int, left:BT[A], right:BT[A]) extends BT[A]
+
+def generateTreeFromList(listOfNodes:List[Int], elementID:Int):BT[Int] = {
+  if(elementID < listOfNodes.length) Node(listOfNodes(elementID), generateTreeFromList(listOfNodes, 2 * elementID + 1), generateTreeFromList(listOfNodes, 2 * elementID + 2))
+  else Empty
+}
 
 //zad1
 
@@ -14,30 +19,38 @@ def generateTree(depth: Int): BT[Int] = {
 //zad2
 
 def multiplyTree(tree: BT[Int]): Int = tree match {
-  case Node(v, l, r) => v * multiplyTree(l) * multiplyTree(r)
   case Empty => 1
+  case Node(v, l, r) => v * multiplyTree(l) * multiplyTree(r)
 }
 
 //zad3
 
-def depthBT[A](tree:BT[A]):List[A] = {
-  def depthBTHelper(stack:List[BT[A]], met: List[A]):List[A] = {
+def depthBT(tree:BT[Int]):List[Int] = {
+  def depthBTHelper(stack:List[BT[Int]], met: List[Int]):List[Int] = {
     stack match {
       case Nil => Nil
       case Empty::tail => depthBTHelper(tail, met)
-      case Node(v, l, r)::tail => if(met.contains(v)) depthBTHelper(List(l, r)++tail, met)
+      case Node(v, l, r)::tail => if(met.contains(v)) (generateNewRandomNumber(met))::depthBTHelper(List(l, r)++tail, met)
       else v::depthBTHelper(List(l, r)++tail, v::met)
     }
   }
   depthBTHelper(List(tree), List())
 }
 
-def breadthBT[A](tree:BT[A]):List[A] = {
-  def breadthBTHelper(queue: List[BT[A]], met: List[A]): List[A] = queue match {
+def breadthBT(tree:BT[Int]):List[Int] = {
+  def breadthBTHelper(queue: List[BT[Int]], met: List[Int]): List[Int] = queue match {
     case Nil => Nil
     case Empty::tail => breadthBTHelper(tail, met)
-    case Node(v, l, r)::tail => if(met.contains(v)) breadthBTHelper(tail++List(l, r), met)
+    case Node(v, l, r)::tail => if(met.contains(v)) (generateNewRandomNumber(met))::breadthBTHelper(tail++List(l, r), met)
     else v::breadthBTHelper(tail++List(l, r), v::met)
   }
   breadthBTHelper(List(tree), List())
 }
+
+def generateNewRandomNumber(duplicates:List[Int]):Int = {
+  val x = util.Random.nextInt(100) + 1
+  if(!duplicates.contains(x)) x
+  else generateNewRandomNumber(duplicates)
+}
+
+
