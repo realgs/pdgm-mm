81525670697c086c108f4b5ba838318293a01ebd
Pawel <mrprzyprawa@gmail.com>
2019-11-24 11:23:31 +0100

    Fix solution of exercise 2

--- Zad2.scala
+++ Zad2.scala
@@ -1,25 +1,25 @@
 object Zad2 extends App {
 
-  def ldzialanie[A] (operation: (A, A) => A, list1: Stream[A], list2:Stream[A], neutralElem: A): Stream[A] =
+  def ldzialanie[A] (operation: (A, A) => A, list1: Stream[A], list2:Stream[A]): Stream[A] =
     (list1, list2) match {
       case (Stream(), Stream()) => Stream()
-      case (Stream.cons(h,t), Stream()) => operation(h, neutralElem) #:: ldzialanie(operation, t, Stream(), neutralElem)
-      case (Stream(), Stream.cons(h,t)) => operation(neutralElem, h) #:: ldzialanie(operation, Stream(), t, neutralElem)
-      case (Stream.cons(h1,t1), Stream.cons(h2,t2)) => operation(h1, h2) #:: ldzialanie(operation, t1, t2, neutralElem)
+      case (_, Stream()) => list1
+      case (Stream(), _) => list2
+      case (Stream.cons(h1,t1), Stream.cons(h2,t2)) => operation(h1, h2) #:: ldzialanie(operation, t1, t2)
     }
 
   val stream1 = Stream(1.0,2.0,3.0)
   val stream2 = Stream(2.0,3.0,4.0,5.0)
 
-  val ldodawanie = ldzialanie((elem1: Double, elem2: Double) => elem1 + elem2, stream1, stream2, 0.0)
+  val ldodawanie = ldzialanie((elem1: Double, elem2: Double) => elem1 + elem2, stream1, stream2)
   println(ldodawanie.force)
 
-  val lodejmowanie = ldzialanie((elem1: Double, elem2: Double) => elem1 - elem2, stream1, stream2, 0.0)
+  val lodejmowanie = ldzialanie((elem1: Double, elem2: Double) => elem1 - elem2, stream1, stream2)
   println(lodejmowanie.force)
 
-  val lmnozenie = ldzialanie((elem1: Double, elem2: Double) => elem1 * elem2, stream1, stream2, 1.0)
+  val lmnozenie = ldzialanie((elem1: Double, elem2: Double) => elem1 * elem2, stream1, stream2)
   println(lmnozenie.force)
 
-  val ldzielenie = ldzialanie((elem1: Double, elem2: Double) => elem1 / elem2, stream1, stream2, 1.0)
+  val ldzielenie = ldzialanie((elem1: Double, elem2: Double) => elem1 / elem2, stream1, stream2)
   println(ldzielenie.force)
 }
