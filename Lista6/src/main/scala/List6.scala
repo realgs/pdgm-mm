961e9f5f4d39dcd314096877e45874173ed1f236
hereak <adam.krzeminski14@gmail.com>
2019-11-25 12:45:17 +0100

    Improved Zad2List6

--- Lista6/src/main/scala/List6.scala
+++ Lista6/src/main/scala/List6.scala
@@ -2,7 +2,7 @@ object List6 extends App {
   //ZADANIE1
   def eachNElement[A](list: LazyList[A], n: Int, lastElem: Int): LazyList[A] = {
     def helper(counter: Int, list: LazyList[A]): LazyList[A] = {
-      if (counter > lastElem - 1 || list==LazyList()) LazyList()
+      if (counter > lastElem - 1 || list == LazyList()) LazyList()
       else if (counter % n == 0) list.head #:: helper(counter + 1, list.tail)
       else helper(counter + 1, list.tail)
     }
@@ -18,25 +18,33 @@ object List6 extends App {
   println(eachNElement(list, 2, 4).toList)
 
   //ZADANIE2
-  def ldzialanie(listOne: LazyList[Int], listTwo: LazyList[Int], operator: Char): LazyList[Int] = {
-    (listOne, listTwo) match {
-      case (LazyList(), _) => listTwo
-      case (_, LazyList()) => listOne
-      case (_, _) => {
-        if (operator == '+') (listOne.head + listTwo.head) #:: ldzialanie(listOne.tail, listTwo.tail, operator)
-        else if (operator == '-') (listOne.head - listTwo.head) #:: ldzialanie(listOne.tail, listTwo.tail, operator)
-        else if (operator == '*') (listOne.head * listTwo.head) #:: ldzialanie(listOne.tail, listTwo.tail, operator)
-        else if (operator == '/') (listOne.head / listTwo.head) #:: ldzialanie(listOne.tail, listTwo.tail, operator)
-        else LazyList()
-      }
+  def chooseOperation(operator: Char):(Int,Int)=>Int={
+    operator match {
+      case '+' => (number1: Int, number2: Int)=> number1 + number2
+      case '-' => (number1: Int, number2: Int)=> number1 - number2
+      case '*' => (number1: Int, number2: Int)=> number1 * number2
+      case '/' => (number1: Int, number2: Int)=> if(number2!=0) number1 / number2 else number1
     }
   }
+  def lOperation(listOne: LazyList[Int], listTwo: LazyList[Int], operator: Char): LazyList[Int] = {
+    def operation=chooseOperation(operator)
 
-  val list1 = LazyList(1, 2, 3)
-  val list2 = LazyList(2, 3, 4, 5)
-  println(ldzialanie(list1, list2, '+').toList)
-  println(ldzialanie(list1, list2, '-').toList)
-  println(ldzialanie(list1, list2, '*').toList)
-  println(ldzialanie(list1, list2, '/').toList)
+    def helper(listOne: LazyList[Int], listTwo: LazyList[Int]): LazyList[Int] = {
+      (listOne, listTwo) match {
+        case (LazyList(), _) => listTwo
+        case (_, LazyList()) => listOne
+        case (_, _) => operation(listOne.head,listTwo.head)#::helper(listOne.tail,listTwo.tail)
+      }
+    }
+
+    helper(listOne, listTwo)
+  }
+
+  val list1 = LazyList(2, 2, 8)
+  val list2 = LazyList(2, 1, 2, 5)
+  println(lOperation(list1, list2, '+').toList)
+  println(lOperation(list1, list2, '-').toList)
+  println(lOperation(list1, list2, '*').toList)
+  println(lOperation(list1, list2, '/').toList)
 
 }
