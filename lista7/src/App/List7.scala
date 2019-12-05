815457a86cfa13b7b91c00089ab851d4f634765a
mprzymus <pzrymus@gmail.com>
2019-12-06 00:38:23 +0100

    task 2 changed

--- lista7/src/App/List7.scala
+++ lista7/src/App/List7.scala
@@ -1,21 +1,25 @@
 package App
 
-import scala.collection.immutable.HashSet
+import scala.annotation.tailrec
+import scala.collection.immutable.{HashSet, ListSet}
 
 class List7 {
 
-  private def generetaNelementsList[A](element : A, size : Int, list : List[A]) : List[A] = {
-    if (size <= 0) list
-    else generetaNelementsList(element, size - 1, element::list)
+  @tailrec
+  private def generateNElementsList[A](element : A, size : Int, list : List[A]) : List[A] = {
+    if (size < 0) throw new IllegalArgumentException("negative number " + size)
+    else if (size == 0) list
+    else generateNElementsList(element, size - 1, element::list)
   }
 
 
   def duplicate[A](toDuplicate : List[A], howManyDuplicate : List[Int]) : List[A] = {
+    @tailrec
     def duplicateHelper(toDuplicate : List[A], duplicationList : List[Int], result : List[A]) : List[A] = {
       if (toDuplicate == Nil) result
       else if (duplicationList == Nil) throw new IllegalArgumentException("duplicationList too short")
       else {
-        val newResult = generetaNelementsList(toDuplicate.head, duplicationList.head, result)
+        val newResult = generateNElementsList(toDuplicate.head, duplicationList.head, result)
         duplicateHelper(toDuplicate.tail, duplicationList.tail, newResult)
       }
 
@@ -23,17 +27,17 @@ class List7 {
     duplicateHelper(toDuplicate, howManyDuplicate, Nil).reverse
   }
 
-  def duplicateNoInputDuplicates[A](toDuplicate : List[A], howManyDuplicate : List[Int]) : List[A] = {
-    def duplicateHelper(toDuplicate : List[A], duplicationList : List[Int], result : List[A], alreadyInList : HashSet[A]) : List[A] = {
-      if (toDuplicate == Nil) result
-      else if (alreadyInList.contains(toDuplicate.head)) throw new IllegalArgumentException("duplicated value " + toDuplicate.head)
-      else if (duplicationList == Nil) throw new IllegalArgumentException("duplicationList too short")
+  def duplicateNoInputDuplicates[A](toDuplicate : ListSet[A], howManyDuplicate : List[Int]) : List[A] = {
+    @tailrec
+    def duplicateHelper(toDuplicate : Set[A], duplicationList : List[Int], result : List[A]) : List[A] = {
+      if (toDuplicate == ListSet()) result
       else {
-        val newResult = generetaNelementsList(toDuplicate.head, duplicationList.head, result)
-        duplicateHelper(toDuplicate.tail, duplicationList.tail, newResult, alreadyInList+toDuplicate.head)
+        require(duplicationList != Nil)
+        val newResult = generateNElementsList(toDuplicate.head, duplicationList.head, result)
+        duplicateHelper(toDuplicate.tail, duplicationList.tail, newResult)
       }
     }
-    duplicateHelper(toDuplicate, howManyDuplicate, Nil, HashSet()).reverse
+    duplicateHelper(toDuplicate, howManyDuplicate, Nil).reverse
   }
 }
 
--- lista7/src/App/test.sc
+++ lista7/src/App/test.sc
@@ -1,14 +1,20 @@
 import App.List7
 
+import scala.collection.immutable.{HashSet, ListSet}
+
 val x = new List7
 
 x.duplicate(List(1,2,3), List(0,3,1,4))
-x.duplicate(List(1,2,3), List(-1,3,1))
+x.duplicate(List(1,2,3), List(1,3,1))
 x.duplicate(List(1,2,3), List(2,3,1))
-x.duplicateNoInputDuplicates(List(1,2,3), List(0,3,1,4))
-x.duplicateNoInputDuplicates(List(1,2,3), List(0,3,3,4))
-x.duplicateNoInputDuplicates(List(1,2,3), List(1,3,2))
-x.duplicateNoInputDuplicates(List(1,2,3), List(2,-2,1,4))
-//x.duplicateNoInputDuplicates(List(1,2,3,2,4), List(0,3,1,4))
-x.duplicateNoInputDuplicates(List("a","b","c"), List(1,2,3))
-x.duplicateNoInputDuplicates(List("a","b","c","a"), List(1,2,3,23,1))
+//x.duplicateNoInputDuplicates(ListSet(1,2,2), List(0,3,1,4))
+
+//val z = new [Int]()
+
+
+x.duplicateNoInputDuplicates(ListSet(1,1), List(0,3,3,4))
+x.duplicateNoInputDuplicates(ListSet(2,1,3), List(1,3,2))
+x.duplicateNoInputDuplicates(ListSet(1,2,3), List(2,2,1,4))
+x.duplicateNoInputDuplicates(ListSet(2,3,2,4), List(0,3,1,4))
+x.duplicateNoInputDuplicates(ListSet("a","b","c"), List(1,2,3))
+x.duplicateNoInputDuplicates(ListSet("a","b","c","a"), List(1,2,3,23,1))
