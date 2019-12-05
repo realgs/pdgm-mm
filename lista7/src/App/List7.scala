ee00b9c93512b0547c4383da9628c248b6d3f13e
mprzymus <pzrymus@gmail.com>
2019-12-05 16:57:32 +0100

    List7 refactor

--- lista7/src/App/List7.scala
+++ lista7/src/App/List7.scala
@@ -3,12 +3,21 @@ package App
 import scala.collection.immutable.HashSet
 
 class List7 {
+
+  private def generetaNelementsList[A](element : A, size : Int, list : List[A]) : List[A] = {
+    if (size <= 0) list
+    else generetaNelementsList(element, size - 1, element::list)
+  }
+
+
   def duplicate[A](toDuplicate : List[A], howManyDuplicate : List[Int]) : List[A] = {
     def duplicateHelper(toDuplicate : List[A], duplicationList : List[Int], result : List[A]) : List[A] = {
       if (toDuplicate == Nil) result
       else if (duplicationList == Nil) throw new IllegalArgumentException("duplicationList too short")
-      else if (duplicationList.head <= 0) duplicateHelper(toDuplicate.tail, duplicationList.tail, result)
-      else duplicateHelper(toDuplicate, duplicationList.head-1::duplicationList.tail, toDuplicate.head::result)
+      else {
+        val newResult = generetaNelementsList(toDuplicate.head, duplicationList.head, result)
+        duplicateHelper(toDuplicate.tail, duplicationList.tail, newResult)
+      }
 
     }
     duplicateHelper(toDuplicate, howManyDuplicate, Nil).reverse
@@ -19,8 +28,10 @@ class List7 {
       if (toDuplicate == Nil) result
       else if (alreadyInList.contains(toDuplicate.head)) throw new IllegalArgumentException("duplicated value " + toDuplicate.head)
       else if (duplicationList == Nil) throw new IllegalArgumentException("duplicationList too short")
-      else if (duplicationList.head > 0) duplicateHelper(toDuplicate, duplicationList.head-1::duplicationList.tail,toDuplicate.head::result,alreadyInList)
-      else duplicateHelper(toDuplicate.tail, duplicationList.tail,result,alreadyInList+toDuplicate.head)
+      else {
+        val newResult = generetaNelementsList(toDuplicate.head, duplicationList.head, result)
+        duplicateHelper(toDuplicate.tail, duplicationList.tail, newResult, alreadyInList+toDuplicate.head)
+      }
     }
     duplicateHelper(toDuplicate, howManyDuplicate, Nil, HashSet()).reverse
   }
--- lista7/src/App/test.sc
+++ lista7/src/App/test.sc
@@ -4,6 +4,7 @@ val x = new List7
 
 x.duplicate(List(1,2,3), List(0,3,1,4))
 x.duplicate(List(1,2,3), List(-1,3,1))
+x.duplicate(List(1,2,3), List(2,3,1))
 x.duplicateNoInputDuplicates(List(1,2,3), List(0,3,1,4))
 x.duplicateNoInputDuplicates(List(1,2,3), List(0,3,3,4))
 x.duplicateNoInputDuplicates(List(1,2,3), List(1,3,2))
