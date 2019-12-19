bd16e4cdf2a2536d024e3e3df0958a762629b3bd
alexHackerNoob <47832121+alexHackerNoob@users.noreply.github.com>
2019-12-19 13:41:30 +0100

    update  lab 8
    add 3

--- lab8.scala
+++ lab8.scala
@@ -1,3 +1,5 @@
+import scala.collection.mutable.ArrayBuffer
+
 trait Debug {
   def debugName():Unit =
   {
@@ -11,7 +13,15 @@ trait Debug {
       println("Field: " + el.getName + " => " + el.getType + ", " + el.get(this))
     })
   }
+  def getArrayOfClassElements():Array[String] =
+  {
+    var bOfElem = ArrayBuffer.empty[String];
+    var declaredFields = getClass.getDeclaredFields
+    declaredFields.foreach(el => {el.setAccessible(true); bOfElem += "Field: " + el.getName + " => " + el.getType + ", " + el.get(this)})
+    bOfElem.toArray;
+  }
 }
+
 class Point(xv: Int, yv: Int) extends Debug {
   var x: Int = xv
   var y: Int = yv
@@ -19,8 +29,11 @@ class Point(xv: Int, yv: Int) extends Debug {
 }
 object Main {
   def main(args: Array[String]): Unit = {
-    var p1 = new Point(12,12)
+    var p1 = new Point(12, 12)
     p1.debugName()
     p1.debugVars()
+    val a = p1.getArrayOfClassElements();
+    println("-------------------");
+    a.foreach(println)
   }
 }
